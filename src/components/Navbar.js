9e26b21151c431822041668043e74a70c3e7eea5
thedon122 <donovan122@yahoo.com>
2021-12-16 16:10:00 +0100

    main and nav style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Navbar() {
-    return ( <h1>Navbar goes here</h1> );
+    return  (
+        <nav>
+            <img src="../images/icon.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav> 
+    );
 };
